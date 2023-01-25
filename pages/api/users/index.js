ed669571ed765e490b5250732b1c05aca8ab8354
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-25 21:01:25 +0000

    Featured: API end point

---
+++ pages/api/users/index.js
@@ -0,0 +1,32 @@
+import connectMongo from '@/database/conn';
+
+export default async function handler(req, res) {
+  connectMongo().catch(() =>
+    res.status(405).json({ error: 'Error in connection' })
+  );
+
+  //Setting up all request
+  const { method } = req;
+
+  switch (method) {
+    case 'GET':
+      res.status(200).json({ method, name: 'GET Request' });
+      break;
+
+    case 'POST':
+      res.status(200).json({ method, name: 'POST Request' });
+      break;
+
+    case 'PUT':
+      res.status(200).json({ method, name: 'PUT Request' });
+      break;
+
+    case 'DELETE':
+      res.status(200).json({ method, name: 'DELETE Request' });
+      break;
+
+    default:
+      res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
